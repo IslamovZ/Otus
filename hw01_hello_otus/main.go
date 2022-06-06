7b191b52e48d1a51b1bd19228984f5652be9246c
islamovz <eldark.islamov@gmail.com>
2022-06-06 16:36:31 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
+const stringForReverse = "Hello, OTUS!"
+
+func printReverseString(str string) {
+	myReversedString := stringutil.Reverse(str)
+	fmt.Println(myReversedString)
+}
+
 func main() {
-	// Place your code here.
+	printReverseString(stringForReverse)
 }
