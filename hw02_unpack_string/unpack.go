5bda7ebd805b3ce062371d52bc47ff166677132d
islamovz <eldark.islamov@gmail.com>
2022-07-11 17:54:54 +0300

    WTAF-2446: fix bug with changing timeframes and hasMoreCandles

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,38 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var unpackedString strings.Builder
+	runeCount := utf8.RuneCountInString(str)
+	runeSliceStr := []rune(str)
+
+	for i := 0; i < runeCount; {
+		curSymbolInRune := runeSliceStr[i]
+
+		if unicode.IsNumber(curSymbolInRune) {
+			return "", ErrInvalidString
+		}
+
+		if i < runeCount-1 && unicode.IsNumber(runeSliceStr[i+1]) {
+			numberOfRepeating := int(runeSliceStr[i+1] - '0')
+
+			for repeatIndex := 0; repeatIndex < numberOfRepeating; repeatIndex++ {
+				unpackedString.WriteRune(curSymbolInRune)
+			}
+			i += 2
+			continue
+		}
+
+		unpackedString.WriteRune(curSymbolInRune)
+		i++
+	}
+
+	return unpackedString.String(), nil
 }
