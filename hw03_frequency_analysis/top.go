ae34315e5c72dc225cd586859a414ea7644385e1
islamovz <eldark.islamov@gmail.com>
2022-10-28 17:56:09 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,8 +1,60 @@
 package hw03frequencyanalysis
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type WordWithCount struct {
+	Word  string
+	Count int
+}
+
+func (wwc *WordWithCount) IncreaseCount() {
+	if wwc == nil {
+		return
+	}
+
+	wwc.Count++
+}
 
 func Top10(bigText string) []string {
-	fmt.Println("vot onoooooooooooooooooo!!!!!!!!!!!!!!!!!!", bigText)
-	return nil
+	words := strings.Fields(bigText)
+	wordsWithCounts := make([]WordWithCount, 0)
+
+	for _, word := range words {
+		founded := false
+		for i, v := range wordsWithCounts {
+			if v.Word == word {
+				wordsWithCounts[i].IncreaseCount()
+				founded = true
+				break
+			}
+		}
+
+		if !founded {
+			wordsWithCounts = append(wordsWithCounts, WordWithCount{word, 1})
+		}
+	}
+
+	sort.Slice(wordsWithCounts, func(i, j int) bool {
+		first := wordsWithCounts[i]
+		second := wordsWithCounts[j]
+
+		if first.Count == second.Count {
+			return first.Word < second.Word
+		}
+
+		return first.Count > second.Count
+	})
+
+	result := []string{}
+	lenArr := len(wordsWithCounts)
+	for i := 0; i < 10 && i < lenArr; i++ {
+		result = append(result, wordsWithCounts[i].Word)
+	}
+
+	fmt.Println(result)
+	return result
 }
